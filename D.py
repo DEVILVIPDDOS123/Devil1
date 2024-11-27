208709ca0c54bf1441140194afaf3b40a3901cb5
DEVILVIPDDOS123 <harbhajansingh666.688@gmail.com>
2024-11-27 21:33:08 +0530

    Update D.py

--- D.py
+++ D.py
@@ -12,7 +12,7 @@ db = client['TEST']
 users_collection = db['PAID']
 
 # Bot Configuration
-TELEGRAM_BOT_TOKEN = '7635290466:AAFmas9eHoWT2i9CtZKNYVD5WnquPMlZW6c'
+TELEGRAM_BOT_TOKEN = '7537496717:AAEw_mSjhfLCSsIzKlZ1FsQdItf-HVms2Lc'
 ADMIN_USER_ID = 6942423757  # Replace with your admin user ID
 ADMIN_USERNAME = 'your_admin_username'  # Replace with your admin's Telegram username (without @)
 
